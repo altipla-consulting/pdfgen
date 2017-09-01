480c9dfe5e4088452e7eb8ea22bf593c46f32239
Alberto Moreno <albertinivva@gmail.com>
2017-09-01 11:25:55 +0200

    Upload the pdf file to Google Storage. (#3)

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 'use strict'
 
-const parseArgs = require('minimist'),
-      puppeteer = require('puppeteer');
+const parseArgs = require('minimist');
+const Storage = require('@google-cloud/storage');
+const puppeteer = require('puppeteer');
 
 
 let options = {
-  string: ['url', 'file'],
+  string: ['url', 'filename', 'bucket'],
   boolean: 'local',
   unknown: () => false,
 };
 let argv = parseArgs(process.argv.slice(2), options);
+
+
+async function uploadFile(data) {
+  let gsoptions = {};
+  if (!argv.local)  {
+    gsoptions.keyFilename = '/etc/service-account.json';
+  }
+
+  let storage = Storage(gsoptions);
+
+  let bucket = storage.bucket(argv.bucket);
+  let file = bucket.file(argv.filename);
+  let stream = file.createWriteStream({
+    metadata: {
+      contentType: 'application/pdf',
+    },
+  });
+
+  return new Promise((resolve, reject) => {
+    stream.on('error', err => reject(err));
+
+    stream.on('finish', () => resolve());
+
+    stream.end(data);
+  });
+}
 
 
 async function main() {
@@ -22,8 +49,9 @@ async function main() {
     format: 'A4',
   });
 
-  console.log(data);
   browser.close();
+
+  await uploadFile(data);
 }
 
 main();
